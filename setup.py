be98757c30bddf2773d391fa344fd53094325a0d
Xavier Lambein <xlambein@gmail.com>
2017-09-28 18:33:03 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='condor',
+      version='0.1',
+      url='https://github.com/xlambein/python-condor',
+      description='A helper for running Python jobs in a condor scheduler',
+      url='http://github.com/storborg/funniest',
+      author='Xavier Lambein',
+      author_email='xlambein@gmail.com',
+      license='MIT',
+      packages=['condor'],
+      zip_safe=False)
+
