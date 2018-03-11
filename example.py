b3a55d35bfb77e727789a52f27d8ac0a3e34a21b
Xavier Lambein <xlambein@gmail.com>
2018-03-11 12:42:50 +0100

    Replaced tabs with 4 spaces

--- example.py
+++ example.py
@@ -4,13 +4,13 @@ import sys
 
 
 def cross_product(xs):
-	if len(xs) == 1:
-		for x in xs[0]:
-			yield (x,)
-	else:
-		for x in xs[0]:
-			for y in cross_product(xs[1:]):
-				yield (x,) + y
+    if len(xs) == 1:
+        for x in xs[0]:
+            yield (x,)
+    else:
+        for x in xs[0]:
+            for y in cross_product(xs[1:]):
+                yield (x,) + y
 
 
 # The different values to try for each parameter
@@ -21,23 +21,23 @@ K = [0.1, 0.2, 0.4]
 
 # Condor jobs launcher
 if not condor.is_condor_job():
-	with condor.SelfJob(
-			output='outputs/output.$(Process)',
-			error='errors/error.$(Process)',
-			log='condor.log',
-			debug=False
-	) as job:
-		for args in cross_product((I, J, K)):
-			job.queue(arguments=args)
+    with condor.SelfJob(
+            output='outputs/output.$(Process)',
+            error='errors/error.$(Process)',
+            log='condor.log',
+            debug=False
+    ) as job:
+        for args in cross_product((I, J, K)):
+            job.queue(arguments=args)
 
 # Condor job
 else:
-	i, j, k = sys.argv[1:]
-	i = int(i)
-	k = float(k)
+    i, j, k = sys.argv[1:]
+    i = int(i)
+    k = float(k)
 
-	a = j * i
-	b = k ** i
+    a = j * i
+    b = k ** i
 
-	print('{}, {}'.format(a, b))
+    print('{}, {}'.format(a, b))
 
